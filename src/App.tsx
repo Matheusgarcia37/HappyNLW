2dcaf8f4a2fd0f8c0937fed12ce5f2e649cc20dc
Matheus Garcia Moreira <matheusgarcia37@hotmail.com>
2020-12-27 14:06:21 -0300

    page-landing finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { FiArrowRight } from 'react-icons/fi';
+
 import "./styles/global.css";
 import "./styles/pages/landing.css";
+
 import logo from './images/Logo.svg';
 
 function App() {
@@ -17,9 +20,11 @@ function App() {
             <strong>Minas gerais</strong>
             <p>Belo horizonte</p>
           </div>
-          <div className="enter-app">
-            <a href="">{'>'}</a>
-          </div>
+         
+            <a href="" className="enter-app">
+              <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)"/>
+            </a>
+          
         </div>
       </div>
     </div>
