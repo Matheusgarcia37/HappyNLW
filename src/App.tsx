d157aecc734bbb35477aee59d4068984e7918353
Matheus Garcia Moreira <matheusgarcia37@hotmail.com>
2020-12-27 13:31:58 -0300

    primeira aula

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./styles/global.css";
+import "./styles/pages/landing.css";
+import logo from './images/Logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="page-landing">
+        <div className="content-wrapper">
+          <img src={logo} alt="Happy" />
+          <main>
+            <h1>Leve felicidade para o mundo</h1>
+            <p>Visite orfanatos e mude o dia de muitas crianças</p>
+          </main>
+          <div id="location">
+            <strong>Minas gerais</strong>
+            <p>Belo horizonte</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
